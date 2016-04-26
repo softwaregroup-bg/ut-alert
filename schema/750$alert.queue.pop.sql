4779f9e2bd8c4da269527e82f80a36e1e27c4bdb
Tanya Rasheva <tanya.rasheva@softwaregroup-bg.com>
2016-04-26 13:53:11 +0300

    fix: change the status of the messages

--- schema/750$alert.queue.pop.sql
+++ schema/750$alert.queue.pop.sql
@@ -3,7 +3,7 @@ ALTER PROCEDURE [alert].[queue.pop] -- returns the specified count of messages o
     @count int -- the number of the messages that should be returned
 AS
 BEGIN TRY
-    DECLARE @statusRequested int = (Select id FROM [alert].[status] WHERE [name] = 'REQUESTED')
+    DECLARE @statusQueued int = (Select id FROM [alert].[status] WHERE [name] = 'QUEUED')
     DECLARE @statusProcessing int = (Select id FROM [alert].[status] WHERE [name] = 'PROCESSING')
 
     update m
@@ -13,7 +13,7 @@ BEGIN TRY
     (
         SELECT TOP (@count) [id]
         FROM [alert].[messageQueue] m
-        WHERE m.[port] = @port AND m.[statusId] = @statusRequested
+        WHERE m.[port] = @port AND m.[statusId] = @statusQueued
         ORDER BY m.[priority] DESC
     ) s
     join [alert].[messageQueue] m on s.Id = m.id
