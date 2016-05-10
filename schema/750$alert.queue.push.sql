2555a0d54196419d08b27281eb8597b60aaa2427
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-05-10 16:25:08 +0300

    feat: refactor metaDataTT

--- schema/750$alert.queue.push.sql
+++ schema/750$alert.queue.push.sql
@@ -2,7 +2,7 @@ ALTER PROCEDURE [alert].[queue.push]
 	@port varchar(255),
     @channel varchar(128),
 	@recipient nvarchar(255),
-	@content nvarchar(max),	
+	@content nvarchar(max),
 	@priority int = 0,
 	@meta [core].[metaDataTT] READONLY
 AS
@@ -10,8 +10,8 @@ BEGIN
 	BEGIN TRY
 		DECLARE @statusName nvarchar(255) = 'QUEUED'
         DECLARE @statusId int = (select id from [alert].[status] where name = @statusName)
-		DECLARE @actorId bigint = (select actorId from @meta)
-		
+		DECLARE @actorId bigint = (select [auth.actorId] from @meta)
+
 		IF @actorId IS NULL
 			RAISERROR(N'alert.systemMessage.add.missingCreatorId', 16, 1);
 
