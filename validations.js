ae889ef34c16f9d4b14c2dd8e4069c820d34d16c
Petar Velchovski <Petar.Velchovski@Softwaregroup-bg.com>
2017-02-08 10:13:02 +0200

    feat-UIS-769: validations

--- validations.js
+++ validations.js
@@ -4,8 +4,17 @@ var joi = require('joi');
 
 module.exports = {
     'push.notification.send': {
-        description: 'Send push notifications to a number of devices',
-        payload: joi.any(),
+        description: 'Send push notifications to a number of devices, specified by an actorId',
+        payload: joi.object().keys({
+            actorId: joi.number().integer().required(),
+            notification: joi.object().keys({
+                firebase: joi.object().keys({
+                    title: joi.string(),
+                    body: joi.string()
+                })
+            }),
+            data: joi.object()
+        }).or('notification', 'data'),
         result: joi.any()
     }
 };
