8e958591f135e263fcba7bad47739d5efcd60ce7
Tanya Rasheva <tanya.rasheva@softwaregroup-bg.com>
2017-07-13 17:24:06 +0300

    fix: change the updates to fix history constraints

--- schema/750$alert.queueOut.notifyFailure.sql
+++ schema/750$alert.queueOut.notifyFailure.sql
@@ -17,13 +17,18 @@ BEGIN TRY
     IF @messageStatus != @statusProcessing AND @messageStatus != @statusFailed
         RAISERROR(N'alert.messageInvalidStatus', 16, 1);
 
+    declare @tmpMessage table(messageId bigint, status varchar(20))
+    
     SELECT 'updated' resultSetName, 1 single;
 
     UPDATE m
     SET [statusId] = @statusFailed
-    OUTPUT INSERTED.id as [messageId], 'FAILED' as [status]
+    OUTPUT INSERTED.id as [messageId], 'FAILED' as [status] into @tmpMessage(messageId, status)
     FROM [alert].[messageOut] m
     WHERE m.[id] = @messageId;
+
+    select messageId, status
+    from @tmpMessage
 END TRY
 BEGIN CATCH
     EXEC core.error
