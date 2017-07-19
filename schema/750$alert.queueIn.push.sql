f39f229c189949f0e6c47af98c735b116a3e532f
Tanya Rasheva <tanya.rasheva@softwaregroup-bg.com>
2017-07-19 19:08:38 +0300

    fix: replace into clause because of the history triggers

--- schema/750$alert.queueIn.push.sql
+++ schema/750$alert.queueIn.push.sql
@@ -17,14 +17,14 @@ BEGIN
         declare @tmp [alert].[messageInTT]
 
         INSERT INTO [alert].[messageIn](port, channel, sender, content, createdOn, statusId, priority)
-        OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, INSERTED.content, INSERTED.createdOn,
+            OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, INSERTED.content, INSERTED.createdOn,
                 @statusName as status, INSERTED.priority
-        INTO @tmp(id, port, channel, sender, content, createdOn, statusId, priority)
+            INTO @tmp(id, port, channel, sender, content, createdOn, statusId, priority)
+        SELECT @port, @channel, @sender, @content, SYSDATETIMEOFFSET(), @statusId, @priority
 
         select id, port, channel, sender, content, createdOn, statusId as status, priority
         from @tmp
-
-        SELECT @port, @channel, @sender, @content, SYSDATETIMEOFFSET(), @statusId, @priority
+        
     END TRY
     BEGIN CATCH
          EXEC [core].[error]
