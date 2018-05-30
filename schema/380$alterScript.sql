587d5e8f87020df2775c0b6848dfeb3c82794600
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2018-05-30 17:13:33 +0300

    encrypt messageIn.content

--- schema/380$alterScript.sql
+++ schema/380$alterScript.sql
@@ -13,3 +13,19 @@ BEGIN
     ALTER TABLE [alert].[messageOut]
         ADD content_Encrypted VARBINARY(max)
 END
+
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.columns c
+    JOIN sys.types y ON y.system_type_id = c.system_type_id
+    WHERE c.Name = 'content' AND Object_ID = OBJECT_ID(N'alert.messageIn') AND y.name = 'varbinary')
+AND NOT EXISTS (
+    SELECT *
+    FROM sys.columns c
+    JOIN sys.types y ON y.system_type_id = c.system_type_id
+    WHERE c.Name = 'content_Encrypted' AND Object_ID = OBJECT_ID(N'alert.messageIn'))
+BEGIN
+    -- Create a column in which to store the encrypted data.
+    ALTER TABLE [alert].[messageIn]
+        ADD content_Encrypted VARBINARY(max)
+END
