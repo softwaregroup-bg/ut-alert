28e5d8ae1610abe7a06818d680c7dcd9f9e65fc9
Rumen Georgiev <rumen.georgiev@softwaregroup-bg.com>
2018-05-30 12:00:50 +0300

    feat: create procedure for read message send to recipient

---
+++ schema/750$alert.queueOut.read.sql
@@ -0,0 +1,18 @@
+ALTER PROCEDURE [alert].[queueOut.read] -- returns decrypted message to a specified recipient
+    @recipient NVARCHAR(255), -- recipient 
+    @statusId INT = 2 -- status o messages (default status is QUEUED)
+AS
+BEGIN TRY
+    DECLARE @sql NVARCHAR(2000) = 'OPEN SYMMETRIC KEY MessageOutContent_Key DECRYPTION BY CERTIFICATE MessageOutContent'
+    EXEC sp_executesql @sql
+
+    SELECT TOP 1 CONVERT(NVARCHAR(1000), DECRYPTBYKEY(m.content, 1 , HASHBYTES('SHA1', CONVERT(VARBINARY, m.id)))) 
+    FROM alert.messageOut m
+    WHERE m.recipient = @recipient
+        AND m.statusId = @statusId
+    ORDER BY id DESC
+
+END TRY
+BEGIN CATCH
+    EXEC core.error
+END CATCH
