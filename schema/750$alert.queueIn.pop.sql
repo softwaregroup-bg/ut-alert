922d78a271afefd3b91181e6dfba60dc28c86cbb
maria-tchaparova <maria.tchaparova@softwaregroup-bg.com>
2018-05-31 16:24:29 +0300

    fix: decrypt MessageIn

--- schema/750$alert.queueIn.pop.sql
+++ schema/750$alert.queueIn.pop.sql
@@ -5,13 +5,14 @@ BEGIN TRY
     DECLARE @statusQueued TINYINT = (SELECT id FROM [alert].[status] WHERE [name] = 'QUEUED')
     DECLARE @statusProcessing TINYINT = (SELECT id FROM [alert].[status] WHERE [name] = 'PROCESSING')
 
-    DECLARE @messageIn TABLE(id BIGINT, port VARCHAR(255), channel VARCHAR(100), sender VARCHAR(255), content VARCHAR(MAX))
+    DECLARE @messageIn TABLE(id BIGINT, port VARCHAR(255), channel VARCHAR(100), sender VARCHAR(255), content NVARCHAR(MAX))
 
-    SELECT 'messages' resultSetName;
+    DECLARE @sql NVARCHAR(2000) = 'OPEN SYMMETRIC KEY MessageOutContent_Key DECRYPTION BY CERTIFICATE MessageOutContent'
+    EXEC sp_executesql @sql
 
     UPDATE m
     SET [statusId] = @statusProcessing
-    OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, INSERTED.content
+    OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, DecryptByKey(INSERTED.content, 1, HashBytes('SHA1', CONVERT(VARBINARY, INSERTED.id)))
     INTO @messageIn (id, port, channel, sender, content)
     FROM
     (
@@ -21,6 +22,8 @@ BEGIN TRY
         ORDER BY m.[priority] DESC
     ) s
     JOIN [alert].[messageIn] m ON s.Id = m.id
+
+    SELECT 'messages' resultSetName;
 
     SELECT id, port, channel, sender, content
     FROM @messageIn
