2f2238952b47dde2ee3408386b262080664abc3c
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2018-05-31 15:25:26 +0300

    decript messageIn

--- schema/750$alert.queueIn.pop.sql
+++ schema/750$alert.queueIn.pop.sql
@@ -9,9 +9,12 @@ BEGIN TRY
 
     SELECT 'messages' resultSetName;
 
+    DECLARE @sql NVARCHAR(2000) = 'OPEN SYMMETRIC KEY MessageOutContent_Key DECRYPTION BY CERTIFICATE MessageOutContent'
+    EXEC sp_executesql @sql
+
     UPDATE m
     SET [statusId] = @statusProcessing
-    OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, INSERTED.content
+    OUTPUT INSERTED.id, INSERTED.port, INSERTED.channel, INSERTED.sender, DecryptByKey(INSERTED.content, 1, HashBytes('SHA1', CONVERT(VARBINARY, INSERTED.id)))
     INTO @messageIn (id, port, channel, sender, content)
     FROM
     (
