74b78b1e5a8af2013d054775025227ae45e5aad3
Dimitar Mihailov <dimitar.mihailov@softwaregroup.com>
2018-06-07 09:30:57 +0300

    SMW-341 Update ut-alert folder structure  * Add joi validations for alert.queueOut.read
    Signed-off-by: Dimitar Mihailov <dimitar.mihailov@softwaregroup.com>

---
+++ validations/queueOut/read.js
@@ -0,0 +1,12 @@
+var joi = require('joi');
+
+module.exports = {
+    description: 'Get latest alert message generated for given recipient',
+    params: joi.object({
+        recipient: joi.string().required(),
+        statusId: joi.string()
+    }),
+    result: joi.object().keys({
+        message: joi.string()
+    })
+};
